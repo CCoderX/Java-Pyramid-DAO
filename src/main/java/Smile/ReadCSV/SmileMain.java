afa237f60d4f6dc353ae2f68b07532bb912f59de
Mohamed Taher <mohmdtaher96@gmail.com>
2021-06-03 19:46:07 +0200

    simple java app to read csv file and

---
+++ src/main/java/Smile/ReadCSV/SmileMain.java
@@ -0,0 +1,11 @@
+package Smile.ReadCSV;
+import Smile.ReadCSV.*;
+public class SmileMain {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+////		read.csv();
+//		System.out.println("hi");
+//	}
+
+}
